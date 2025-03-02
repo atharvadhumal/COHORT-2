b789345116b8a0edce63dacafb281c79470bf8b9
Atharva Dhumal <atharvaadhumal@gmail.com>
2025-03-02 17:08:47 +0530

    leanrned about react foundation useStates , etc

---
+++ week-5/counter-app/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App() {
+  
+  const [todos, setTodos] = useState([{
+    title: "Go to Gym",
+    Description: "Go to gym from 7 to 9",
+     completed: false
+  },
+  {
+    title: "Study DSA",
+    Description: "solve 5 problems",
+    completed: true
+
+  }]); //defines the initial state which means it will start from 0s
+  
+  return (
+
+    <div>
+     {todos.map(function(todo){
+      return 
+      <Todos title={todos.title} Description={todo.Description} />
+     })}
+    </div>
+  )
+}
+
+function Todos(props) {
+
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.Description}</h2>
+  </div>
+}
+
+export default App
