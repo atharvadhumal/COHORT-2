bd0001404f38d8cd3a37a12b4caacbad7e86888e
Atharva Dhumal <atharvaadhumal@gmail.com>
2025-03-29 23:15:41 +0530

    learned about useState, useEffect, useMemo, useRef

--- week-6/6.3/src/App.jsx
+++ week-6/6.3/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const divRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      divRef.current.innerHTML = "10" //getting current value inside specific div
+    }, 5000);
+  }, [])
+
+  const incomeTax = 20000;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        hi there, your income tax returns are <div ref={divRef}>{incomeTax}</div>
+    </div>
   )
 }
 
