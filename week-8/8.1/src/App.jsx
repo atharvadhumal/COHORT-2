4267af1a50bfd599959b8b83884d89389f865525
atharvadhumal <atharvaadhumal@gmail.com>
2025-07-04 11:45:01 +0530

    learned about flex and grid in tailwind and how to use them

--- week-8/8.1/src/App.jsx
+++ week-8/8.1/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <div style={{display: 'flex', justifyContent: 'center'}}> {/*this is how we write css text in react
+        <div style={{backgroundColor: 'red'}}>hi</div>
+        <div style={{backgroundColor: 'blue'}}>hi</div>
+        <div style={{backgroundColor: 'green'}}>hi</div>
+      </div> */}
+
+        <div className='grid grid-cols-3'> {/* this grid will have 3 columns if 4 is added it will go below the 1st */}
+          <div className='bg-red-500'>hi</div>
+          <div className='bg-blue-500'>hi</div>
+          <div className='bg-green-500'>hi</div>
+          <div className='bg-green-500'>hi</div>
+        </div>
     </>
   )
 }
